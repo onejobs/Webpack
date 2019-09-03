ddbb2aacf1571480e2bf2d969ce0342d77256198
wdd <onejobs@163.com>
2019-09-03 15:29:08 +0800

    day01

---
+++ 04-code/webpack-demo/src/calc.js
@@ -0,0 +1,4 @@
+// 提供简单计算功能
+const calc = (x, y) => x + y
+
+export default calc
---
+++ 04-code/webpack-demo/src/index.js
@@ -0,0 +1,9 @@
+import "@babel/polyfill";
+// 入口文件
+import calc from './calc'
+
+import './assets/css/index.css'
+
+import './styles/index.less'
+
+console.log(calc(10, 20))
